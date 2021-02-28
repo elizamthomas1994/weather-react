a03d95597f38b6c66691211ed1a8053743c2d46e
elizamthomas1994 <78053561+elizamthomas1994@users.noreply.github.com>
2021-02-28 16:12:04 +0000

    Attempted to correct Netlifly compatibility error

--- src/Coder.js
+++ src/Coder.js
@@ -2,6 +2,6 @@ import React from "react";
 import './Coder.css';
 
 export default function Coder() {
-  return <p><a href="https://github.com/elizamthomas1994/weather-react" target="_blank">Open source code</a> by Eliza
+  return <p><a href="https://github.com/elizamthomas1994/weather-react" target="_blank" rel="noreferrer">Open source code</a> by Eliza
             Thomas</p>
 }
